b7336a3235d2292ce8cfabcf1453bf8386529fc2
Andrew Monks <a@monks.co>
2017-02-11 14:48:59 -0600

    fix bash logic

--- __gen__/init.sh
+++ __gen__/init.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/env sh
 
 # cd into __gen__ if we haven't already
-if [ $(basename `pwd` != '__gen__') ]
+if [ $(basename `pwd`) != '__gen__' ]
 then
   cd __gen__
-  if [$(basename `pwd` != '__gen__') ]
+  if [$(basename `pwd`) != '__gen__' ]
   then
     exit 1
   fi
