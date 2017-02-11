3d91840ba88f28e715e539ebf133c4c76fb4ae2e
Andrew Monks <a@monks.co>
2017-02-11 07:31:55 -0600

    comments, fix

--- __gen__/gen.js
+++ __gen__/gen.js
@@ -2,10 +2,10 @@ const { ask, replacer, tag: getOrigin } = require('gen-util')
 const $ = require('shelljs')
 
 const main = async () => {
-  // cwd
+  console.log('cwd')
   await $.cd(`${__dirname}/..`)
 
-  // config
+  console.log('config')
   const origin = await getOrigin()
   const name = await ask('project npm name?')
   const repo = await ask('project repo? (arg to `git create`)')
@@ -13,27 +13,26 @@ const main = async () => {
   const currentYear = new Date().getFullYear()
   const config = {name, repo, author, origin, currentYear}
 
-  // write log
+  console.log('write log')
   $.ShellString(JSON.stringify(config, undefined, 2))
     .to(`__gen__.json`)
 
-  // remove git
-  await $.rm('-rf .git')
+  console.log('remove git')
+  await $.rm('-rf', '.git')
 
-  // do replacements
+  console.log('do repalcements')
   const files = $.find(__dirname)
   const replace = replacer(files)
   await replace(/__CURRENT_YEAR/, currentYear)
   await replace(/__PROJECT_NAME/, name)
   await replace(/__AUTHOR_NAME/, author)
 
-  // make new git
+  console.log('make new git')
   await $.exec('git init')
   await $.exec(`git create ${repo}`)
   await $.exec('git add .')
   await $.exec("rit commit -am 'Initial commit'")
 
-  // all done
   console.log('💥')
 }
 
